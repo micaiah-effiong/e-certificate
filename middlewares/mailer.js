634403a7e00703be85fd0fcd1c7933e070fdd34f
Artalia <micaiah.effiong@gmail.com>
2020-03-27 17:44:33 +0100

    update message properties

--- middlewares/mailer.js
+++ middlewares/mailer.js
@@ -1,37 +1,27 @@
 const nodemailer = require('nodemailer');
 
 module.exports = function (req, res, next) {
-	console.log(req.query, (!!req.query.sendToMail));
 	if (!!req.query.sendToMail == false ||
 		!!req.body.sendMail == false) return next();
 
-	console.log('creating account');
-
-	console.log('creating transport');
 	const transpoter = nodemailer.createTransport({
-		/*host: 'smtp.ethereal.email',
-		port: 587,
-		secure: false,*/
 		tls: {rejectUnauthorized: false},
 		service: 'gmail',
 		auth: {
 			user: "micaiah.effiong@gmail.com",
-			pass: ''
+			pass: process.env.emailPass
 		}
 	});
-	// let message = Object.create(null);
-	console.log('creating message');
+
 	let message = {
 		from: "Micaiah Effiong",
-		to: req.mailing.userEmail,
+		to: req.mailing.to,
 		subject: req.mailing.subject || req.mailing.title,
 		text: req.mailing.text || "",
 		html: req.mailing.html || "",
 		attachments: [req.mailing.attachments] || []
 	}
-	console.log('sending message');
 	transpoter.sendMail(message).then(function(info){
-		console.log('done with mailer', info);
 		next();
 	}).catch(console.error);
 }
