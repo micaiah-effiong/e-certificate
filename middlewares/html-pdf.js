9c9a1e5b56095a8693cc19eb87ef8b11f875aa2b
Artalia <micaiah.effiong@gmail.com>
2020-03-27 17:36:49 +0100

    set html-pdf in middleware folder for proper placement

---
+++ middlewares/html-pdf.js
@@ -0,0 +1,35 @@
+const toPdf = require('html-pdf'),
+	fs = require('fs'),
+	ejs = require('ejs');
+
+module.exports = function(req, res, next){
+
+
+	/*
+	fetch('localhost:3300/certificate')
+		.then(function(_res){
+			console.log(_res)
+		}, function(e){
+			console.log(e)
+		});
+	*/
+
+	// if(req.user.completed == false) return res.status(401).send();
+
+	let template = fs.readFileSync(require.resolve('../public/certificate.html'), 'utf8');
+	toPdf.create(template, {format: 'A5', orientation: 'landscape'}).toBuffer(function(err, buffer){
+		if (err) return res.send(err.stack);
+		res.set('Content-type', 'application/pdf');
+		req.mailing = {
+			to: "micaiah.effiong@gmail.com", // req.body.email
+			subject: "certificate of completion",
+			attachments : {
+				filename: "certificate.pdf",
+				content: buffer
+			}
+		};
+
+		req.userCertBuffer = buffer;
+		next();
+	});
+}
