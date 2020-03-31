0eac70fb424a44fbf279eeced39eb310a9407b67
Artalia <micaiah.effiong@gmail.com>
2020-03-31 16:04:32 +0100

    update middleware to fit current database structure while checking for completed courses

--- middlewares/user-details.js
+++ middlewares/user-details.js
@@ -3,14 +3,23 @@ module.exports = function (db) {
 		completedCourse: function(req, res, next){
 			db.user.findOne({
 				where: {
-					regNo: req.query.key || req.body.key,
-					completed: true
+					regNo: req.query.key || req.body.key
 				}
 			}).then(function(user){
-				if (user) return req.user = user.toJSON();
-				res.status(404).send();
-			}, function(){
-				res.status(500).send();
+				if (!user) return res.status(404).send();
+					req.user = user;
+					user.getCourses().then(function(courses){
+						req.user.completedCourse = courses.filter(a=>{
+							if(a.toJSON().completed){
+								return a.toJSON();
+							}
+						});
+						next();
+					}, function(e){
+						return e;
+					});
+			}, function(e){
+				res.status(500).send(e);
 			});
 		},
 	}
