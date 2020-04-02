5d4678b529d0adf40b9c9347ad676e0828ca2686
Artalia <micaiah.effiong@gmail.com>
2020-04-02 19:10:27 +0100

    bug fixes

--- middlewares/html-pdf.js
+++ middlewares/html-pdf.js
@@ -2,26 +2,23 @@ const toPdf = require('html-pdf'),
 	fs = require('fs'),
 	ejs = require('ejs');
 
+const STATIC_FILES = {
+	css: 'style.css',
+	img: ['tech101 certification of completion.png', 'tech 101 sponsorers.png', 'signature.png']
+}
+
 module.exports = function(req, res, next){
 
-
-	/*
-	fetch('localhost:3300/certificate')
-		.then(function(_res){
-			console.log(_res)
-		}, function(e){
-			console.log(e)
-		});
-	*/
-
 	// if(req.user.completed == false) return res.status(401).send();
-
 	let template = fs.readFileSync(require.resolve('../public/certificate.html'), 'utf8');
-	toPdf.create(template, {format: 'A5', orientation: 'landscape'}).toBuffer(function(err, buffer){
+	let html = template.replace(/{{name}}/g, `${req.user.firstname} ${req.user.lastname}`);
+	// console.log(html, req.user.firstname);
+
+	toPdf.create(html, {format: 'A5', orientation: 'landscape'}).toBuffer(function(err, buffer){
 		if (err) return res.send(err.stack);
 		res.set('Content-type', 'application/pdf');
 		req.mailing = {
-			to: "micaiah.effiong@gmail.com", // req.body.email
+			to: req.user.email, // req.body.email
 			subject: "certificate of completion",
 			attachments : {
 				filename: "certificate.pdf",
