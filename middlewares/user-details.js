ea809e348cf021f72f40bec0c40b7a2335f2903f
micaiah-effiong <micaiah.effiong@gmail.com>
2020-05-10 20:30:05 +0100

    checking if client request contains a valid key as user registration number

--- middlewares/user-details.js
+++ middlewares/user-details.js
@@ -1,6 +1,10 @@
 module.exports = function (db) {
 	return {
 		completedCourse: function(req, res, next){
+			if (!(req.query.key || req.body.key)) {
+				return res.status(401).json({error: 'invalid request'})
+			}
+
 			db.user.findOne({
 				where: {
 					regNo: req.query.key || req.body.key
@@ -9,6 +13,8 @@ module.exports = function (db) {
 				if (!user) return res.status(404).send();
 					req.user = user;
 					user.getCourses().then(function(courses){
+						console.log(courses.length, !courses);
+						if (!courses) return res.status(401).send(e);
 						req.user.completedCourse = courses.filter(a=>{
 							if(a.toJSON().completed){
 								return a.toJSON();
