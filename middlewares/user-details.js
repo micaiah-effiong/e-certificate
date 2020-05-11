b5b3a5a10efd6cd026ebda9c757c44eb0d33b237
micaiah-effiong <micaiah.effiong@gmail.com>
2020-05-11 10:54:06 +0100

    created a function to handle request for completed courses and separeted completed courses from certificate varification

--- middlewares/user-details.js
+++ middlewares/user-details.js
@@ -1,32 +1,46 @@
 module.exports = function (db) {
+
+	function getCompletedCourse(req, res, next, id){
+		db.user.findOne({
+			where: {
+				regNo: id
+			}
+		}).then(function(user){
+			if (!user) return res.status(404).send();
+				req.user = user;
+				user.getCourses().then(function(courses){
+					console.log(courses.length, !courses);
+					if (!courses) return res.status(404).send(e);
+					req.user.completedCourse = courses.filter(course=>{
+						if(course.toJSON().completed){
+							return course.toJSON();
+						}
+					});
+					next();
+				}, function(e){
+					return e;
+				});
+		}, function(e){
+			res.status(500).send(e);
+		});
+	}
+
 	return {
 		completedCourse: function(req, res, next){
-			if (!(req.query.key || req.body.key)) {
-				return res.status(401).json({error: 'invalid request'})
+			if (!(req.query.email || req.body.email)) {
+				return res.status(400).json({error: 'invalid request'})
 			}
 
-			db.user.findOne({
-				where: {
-					regNo: req.query.key || req.body.key
-				}
-			}).then(function(user){
-				if (!user) return res.status(404).send();
-					req.user = user;
-					user.getCourses().then(function(courses){
-						console.log(courses.length, !courses);
-						if (!courses) return res.status(401).send(e);
-						req.user.completedCourse = courses.filter(a=>{
-							if(a.toJSON().completed){
-								return a.toJSON();
-							}
-						});
-						next();
-					}, function(e){
-						return e;
-					});
-			}, function(e){
-				res.status(500).send(e);
-			});
+			let id = req.query.key || req.body.key;
+			getCompletedCourse(req, res, next, id);
 		},
+		verifyCert: function(req, res, next){
+			if (!(req.query.key || req.body.key)) {
+				return res.status(400).json({error: 'invalid request'})
+			}
+
+			let id = req.query.key || req.body.key;
+			getCompletedCourse(req, res, next, id);
+		}
 	}
 }
