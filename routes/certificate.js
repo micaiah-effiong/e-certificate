14eb6adbcc63bb1b2793e11ce854310b58f4c009
micaiah-effiong <micaiah.effiong@gmail.com>
2020-05-11 10:54:58 +0100

    created certificate route

---
+++ routes/certificate.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const db = require('../db/db');
+const pdf = require('../middlewares/html-pdf');
+const mailer = require('../middlewares/mailer');
+const stuDetails = require('../middlewares/user-details')(db);
+const router = express.Router();
+const path = require('path');
+
+router.use(express.static(path.join(__dirname, '..', 'public')));
+
+router.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '..', 'public', 'certificate.html'));
+});
+
+router.get('/get', stuDetails.completedCourse, pdf, mailer, function(req, res){
+  res.send(req.userCertBuffer);
+});
+
+/*may not be used*/
+router.get('/download', stuDetails.completedCourse, pdf, function(req, res){
+  res.download(req.userCertBuffer);
+});
+
+router.get('/verify', stuDetails.verifyCert, function(req, res){
+  res.json({
+    coures: req.user.completedCourse,
+    user: req.user
+  });
+});
+
+module.exports = router;
+
+// get cert with email
+// verify cert with regNo
