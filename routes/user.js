6e7d4b27ea39ec09c088438e774b72212ef9e3da
micaiah-effiong <micaiah.effiong@gmail.com>
2020-05-17 09:31:28 +0100

    added endpoints (get all users, and logout)

--- routes/user.js
+++ routes/user.js
@@ -6,19 +6,51 @@ const db = require('../db/db');
 const auth = require('../middlewares/authenticate')(db);
 const pdf = require('../middlewares/html-pdf');
 const stuDetails = require('../middlewares/user-details')(db);
+const errorResponse = require('../handlers/error');
 const router = express.Router();
 
-// POST /create
+router.get('/', auth.authToken, (req, res)=>{
+  db.user.findAll()
+    .then(users=>{
+      if(!users) return res.status(404).json({
+        success: false,
+        msg: 'users not found'
+      });
+      let result = users.map(user=>{
+        return user.toPublicJSON();
+      });
+      return res.json({
+        success: true,
+        data: result
+      });
+    })
+    .catch(err=>{
+      res.status(500).json({
+        success: false,
+        msg: 'An error occured while fetching users',
+        err: err
+      });
+    })
+});
 
-/*
-  Authenticate user
-*/
+router.get('/profile', auth.authToken, (req, res)=>{
+  res.send('req.cookies');
+});
 
-// GET /:id
-// PUT /edit/:id
-// DELETE /:id
+router.get('/logout', (req, res)=>{
+  console.log('login out')
+  res.cookie('serialized', {
+    isIn: false,
+    'x-token': null
+  });
+  res.json({
+    success: true,
+    msg: 'User has logged out'
+  });
+});
 
-router.post('/register', function(req, res){
+
+router.post('/', function(req, res){
   db.user.createRegKey(req).then(function(){
     db.user.create(req.body).then(function(user){
       res.status(201).json(user.toPublicJSON());
@@ -42,36 +74,97 @@ router.post('/login', function(req, res){
   })
   .then(user=>{
     return bcrypt.compare(req.body.password, user.get('hash'))
-    .then(result=>user)
+    .then(result=>{
+      return {user, result}
+    });
   })
-  .then(user=>{
-    return user.generateToken()
-    .then(token=>{
-      return {
-        user,
-        token
-      }
-    })
+  .then(info=>{
+    if (info.result) {
+      let {user} = info;
+      return user.generateToken()
+      .then(token=>{
+        return {
+          user,
+          token
+        }
+      })
+    }
+    return new Error("Invalid credentials");
   })
   .then(userToken=>{
     res.cookie('serialized', {
       serialized: userToken.user.get('id'),
       isIn: true,
       'x-token': userToken.token
-    }).json(userToken.user.toPublicJSON());
+    }).json({
+      success: true,
+      msg: 'User has logged in',
+      data: userToken.user.toPublicJSON()
+    });
   })
   .catch(e=>{
-    res.status(500).send(e);
+    res.status(400).json({
+      success: false,
+      err: e,
+      msg: "Could not login user due to Invalid credentials"
+    });
   });
 });
 
-router.get('/profile', auth.authToken, (req, res)=>{
-  res.send('req.cookies');
+router.put('/:id', auth.authToken, (req, res)=>{
+  db.user.findOne({where:{id: req.param.id}})
+    .then(user=>{
+      if(!user) return res.status(400).json({
+        status: false,
+        msg: 'no valid user with such ID'
+      });
+      return user;
+    })
+    .then(user=>{
+      db.user.update(req.body)
+        .then(result=>{
+          res.json({
+            success: true,
+            msg: 'User has been updated',
+            data: result.toPublicJSON()
+          })
+        });
+    })
+    .catch(err=>{
+      res
+        .status(500)
+        .json({
+          success: false,
+          msg: 'Error occured while trying to update user details',
+          err: err
+        })
+    })
 });
 
-router.get('/logout', (req, res)=>{
-  res.clearCookie('serialized');
-  res.send('Thanks for getting out');
+router.delete('/:id', auth.authToken, (req, res)=>{
+  
+});
+
+router.get('/:id', auth.authToken, (req, res)=>{
+  let id = +(req.params.id);
+  return db.user.findByPk(id)
+    .then(user=>{
+      if(!user) return res.status(404).json({
+        success: false,
+        msg: 'user not found'
+      });
+      return res.json({
+        success: true,
+        data: user
+      });
+    })
+    .catch(err=>{
+      res.status(400).json({
+        success: false,
+        msg: 'An error occured while fetching user',
+        err: err
+      });
+    })
 });
 
 module.exports = router;
