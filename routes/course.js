71d21cff33520b917f00145f1043a48912ca81a6
micaiah-effiong <micaiah.effiong@gmail.com>
2020-05-18 15:09:19 +0100

    created course route POST /

--- routes/course.js
+++ routes/course.js
@@ -1,9 +1,27 @@
-// POST /create
+const express = require('express');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const db = require('../db/db');
+const auth = require('../middlewares/authenticate')(db);
+const pdf = require('../middlewares/html-pdf');
+const stuDetails = require('../middlewares/user-details')(db);
+const errorResponse = require('../handlers/error');
+const router = express.Router();
 
-/*
-  Authenticate user
-*/
+router.post('/', auth.authToken, auth.authEmail, function(req, res){
+  db.course.create(req.body)
+  .then(function(course){
+    req.user.addCourse(course).then(function(){
+      return course.reload();
+    }).then(function(course){
+      res.json(course.toJSON());
+    }, function(e){
+      res.status(500).send(e||null);
+    });
+  }, function(e){
+    res.status(500).send(e||null);
+  });
+});
 
-// GET /:id
-// PUT /edit/:id
-// DELETE /:id
+module.exports = router;
