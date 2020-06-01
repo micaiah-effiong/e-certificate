e101441f24f147f747f54103c37b9df80da58642
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-01 20:57:24 +0100

    Refactor: use arrow fuctions

--- middlewares/html-pdf.js
+++ middlewares/html-pdf.js
@@ -3,16 +3,16 @@ const errorResponse = require('../handlers/error');
 const fs = require('fs');
 const ejs = require('ejs');
 
-module.exports = function(req, res, next){
+module.exports = (req, res, next)=>{
 	fs.readFile(require.resolve('../views/index.ejs'), 'utf8', (err, template)=>{
-		if (err) return console.log(err);
+		if (err) return next(errorResponse('Server error', 500));
 		let html = ejs.render(template, {
 			name: req.user.getFullName(),
 			courses: req._completedCourse,
 			regNo: `Validation No.: ${req.user.regNo}`
 		});
 
-		toPdf.create(html, {format: 'A5', orientation: 'landscape'}).toBuffer(function(err, buffer){
+		toPdf.create(html, {format: 'A5', orientation: 'landscape'}).toBuffer((err, buffer)=>{
 			if (err) return next(errorResponse('Server error', 500));
 			res.set('Content-type', 'application/pdf');
 			req.mailing = {
