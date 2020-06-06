ebebc7a10ce50ea074ebe766362697533ad727bf
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-06 08:44:13 +0100

    bug: fix email spam bug using google oauth

--- middlewares/mailer.js
+++ middlewares/mailer.js
@@ -4,7 +4,7 @@ const errorResponse = require('../handlers/error');
 
 module.exports = function (req, res, next) {
 	if (!(req.query.sendToMail
-		|| req.body.sendMail)) return next();
+		|| req.body.sendToMail)) return next();
 	jwt.verify(process.env.EMAIL_PASS, process.env.EMAIL_PASS_KEY, (err, pwd)=>{
 
 		if (err){
@@ -14,10 +14,24 @@ module.exports = function (req, res, next) {
 		const transpoter = nodemailer.createTransport({
 			tls: {rejectUnauthorized: false},
 			service: 'gmail',
+			host: 'smtp.gmail.com',
+	    port: 465,
+	    secure: false,
 			auth: {
+				type: 'oauth2',
 				user: process.env.EMAIL,
-				pass: pwd
+		    clientId: process.env.CLIENT_ID,
+		    clientSecret: process.env.CLIENT_SECRET,
+		    refreshToken: process.env.REFRESH_TOKEN,
+		    "expires_in": 3599,
+		    accessToken: process.env.ACCESS_TOKEN,
 			}
+		});
+
+		transpoter.set('oauth2_provision_cb', (user, renew, callback)=>{
+			let accessToken = userTokens[user];
+			if (!accessToken) return callback(new Error('Unknown user'));
+			return callback(null, accessToken)
 		});
 
 		let message = {
