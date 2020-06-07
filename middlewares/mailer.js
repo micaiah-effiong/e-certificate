60905bc7d6d2cad819f69d60ac96cdd216183632
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-07 21:37:44 +0100

    bug: fix using less secure, remove email password from app and authenticate user with Oauth

--- middlewares/mailer.js
+++ middlewares/mailer.js
@@ -1,60 +1,46 @@
-const nodemailer = require('nodemailer');
-const jwt = require('jsonwebtoken');
-const errorResponse = require('../handlers/error');
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+const errorResponse = require("../handlers/error");
 
 module.exports = function (req, res, next) {
-	if (!(req.query.sendToMail
-		|| req.body.sendToMail)) return next();
-	jwt.verify(process.env.EMAIL_PASS, process.env.EMAIL_PASS_KEY, (err, pwd)=>{
+  if (!(req.query.sendToMail || req.body.sendToMail)) return next();
 
-		if (err){
-			return next(errorResponse('Could not send mail', 500));
-		}
+  const transpoter = nodemailer.createTransport({
+    // tls: {rejectUnauthorized: false},
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      type: "oauth2",
+      user: process.env.EMAIL,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      expires_in: 3599,
+      accessToken: process.env.ACCESS_TOKEN,
+    },
+  });
 
-		const transpoter = nodemailer.createTransport({
-			tls: {rejectUnauthorized: false},
-			service: 'gmail',
-			host: 'smtp.gmail.com',
-	    port: 465,
-	    secure: false,
-			auth: {
-				type: 'oauth2',
-				user: process.env.EMAIL,
-		    clientId: process.env.CLIENT_ID,
-		    clientSecret: process.env.CLIENT_SECRET,
-		    refreshToken: process.env.REFRESH_TOKEN,
-		    "expires_in": 3599,
-		    accessToken: process.env.ACCESS_TOKEN,
-			}
-		});
+  transpoter.set("oauth2_provision_cb", (user, renew, callback) => {
+    let accessToken = userTokens[user];
+    if (!accessToken) return callback(new Error("Unknown user"));
+    return callback(null, accessToken);
+  });
 
-		transpoter.set('oauth2_provision_cb', (user, renew, callback)=>{
-			let accessToken = userTokens[user];
-			if (!accessToken) return callback(new Error('Unknown user'));
-			return callback(null, accessToken)
-		});
+  let message = {
+    from: process.env.ORG_NAME,
+    to: req.mailing.to,
+    subject: req.mailing.subject || req.mailing.title || "",
+    text: req.mailing.text || "",
+    html: req.mailing.html || "",
+    attachments: [req.mailing.attachments] || [],
+  };
 
-		let message = {
-			from: process.env.APP_ORG,
-			to: req.mailing.to,
-			subject: req.mailing.subject || req.mailing.title,
-			text: req.mailing.text || "",
-			html: req.mailing.html || "",
-			attachments: [req.mailing.attachments] || []
-		}
-		transpoter.sendMail(message)
-		.then(info=>{
-			next();
-		})
-		.catch(err=>next(err));
-	});
-}
-
-/*
-	1. From
-	2. To
-	3. Subject <Title>
-	4. Text
-	5. Html
-	6. Attachment <certificate pdf>
-*/
+  transpoter
+    .sendMail(message)
+    .then((info) => {
+      next();
+    })
+    .catch((err) => next(err));
+};
