a5b7bff9bdd194eafb8581c4ef277a88bf1f8625
Akanimorex <akanimoime2016@gmail.com>
2020-07-08 14:21:42 +0100

    desktop version

--- public/src/pages/login_and_signup/Login.js
+++ public/src/pages/login_and_signup/Login.js
@@ -7,7 +7,7 @@ class Login extends Component {
         return (
             <div>
                 <div className="row">
-                    <div className="col-md-4" style={{backgroundColor:'#5F07B1',height:'100vh'}}>
+                    <div className="col-md-3" style={{backgroundColor:'#5F07B1',height:'100vh'}}>
                         <div className="container">
                             <div className="side-bar">
                                 <h2>Taking a step<br></br> to simplicity</h2>
@@ -18,8 +18,9 @@ class Login extends Component {
 
 
                     </div> 
-                    <div className="col-md-8" style={{backgroundColor:'#9b51e0'}}>
+                    <div className="col-md-9" style={{backgroundColor:'#9b51e0'}}>
                         <div className="container">
+                        <a href="#" className="fa fa-arrow-left back-home-link"> Back to Home</a>
                             <div className="card">
                                 <h4>Login</h4>
 
@@ -29,13 +30,13 @@ class Login extends Component {
                                     <label>Password</label>
                                     <input type="password"/>
                                     <div className="checkbox-div">
-                                        <input   type="checkbox" name="remember me" /> 
+                                        <input type="checkbox" name="remember me" />Remember me 
                                     </div>
                                     
                                     <br></br>
                                     <div style={{textAlign:'center'}}>
                                         <button className="login-btn" type="submit">Login</button>
-                                        <p>Or</p>
+                                        <p style={ {fontWeight:'bold',margin:'20px' } }>Or</p>
                                         <button className="google-btn" type="submit">Login With Google</button>
                                         
                                     </div>
@@ -45,6 +46,8 @@ class Login extends Component {
                                 </form>
 
                             </div>
+
+                            <p className="foot-register-link">Don't have an account?<a href="#"> Register here</a></p>
 
                         </div>
                     </div>
