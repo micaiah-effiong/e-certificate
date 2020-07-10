06d41d43982af4d6e900bd89109c2252944c5b30
Akanimorex <akanimoime2016@gmail.com>
2020-07-10 15:42:08 +0100

    user can edit profile

---
+++ public/src/components/Userprofile.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './userprofile.css'
+
+
+const Userprofile =()=>{
+    return (
+        <div>
+
+            <div>
+                <div>
+                    <div>
+                         
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Userprofile;
