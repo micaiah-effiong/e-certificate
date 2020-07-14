c011cb0266c666ce898c868c5ecbd8ec0518231f
Akanimorex <akanimoime2016@gmail.com>
2020-07-14 15:42:26 +0100

    interface where users can edit profile

--- public/src/components/Userprofile.js
+++ public/src/components/Userprofile.js
@@ -1,19 +1,74 @@
 import React from 'react';
-import './userprofile.css'
+import SideNav from './SideNav';
+import './userprofile.css';
+import ellipse from '../img/ellipse.svg';
+
 
 
 const Userprofile =()=>{
     return (
-        <div>
+        <div className="container-fluid">
+            <SideNav/>
+            <div className="main">
 
-            <div>
-                <div>
-                    <div>
-                         
-                    </div>
+                <div className="dashboard-header">
+                    <p>User Profile</p>
                 </div>
-            </div>
+                 
+               
 
+                <div className="card">
+                    <div className="avatar-section">
+                        <img src={ellipse} alt="profile picture" />
+                        <div className="avatar-btn-wrap">
+                            <button className="fake-btn">Change Avatar</button>
+                            {/* <input type="file" size="10" /> */}
+                        </div>                       
+                    </div>
+                    <div className="vl"></div>
+                    <div className="">
+                        <form className="form-group">
+                            <div className="row">
+
+                                <div className="col-md-6">
+                                    <label>First Name</label>
+                                    <input type="text" className="form-control"/>
+                                </div>
+                                <div className="col-md-6">
+                                <label>Last Name</label>
+                                    <input type="text" className="form-control"/>
+                                </div>
+                            </div>
+                            <div className="row">
+                            <div className="col-md-6">
+                                    <label>Other Names</label>
+                                    <input type="text" className="form-control"/>
+                                </div>
+                                <div className="col-md-6">
+                                <label>Email</label>
+                                    <input type="email" className="form-control"/>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <label>Location</label>
+                                    <input type="text" className="form-control" />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <button className="fake-btn" type="submit">Edit</button>
+                                </div>
+                            </div>
+                        
+                        </form>
+
+                    </div>
+
+
+                </div>
+
+            </div>
         </div>
     )
 }
