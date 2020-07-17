da2621ee59c9e6d3fb0270a3fe7c04a89ed9c879
Akanimorex <akanimoime2016@gmail.com>
2020-07-17 10:29:27 +0100

    fully responsive now:next,conditional rndering for courses

--- public/src/components/Dashboard.js
+++ public/src/components/Dashboard.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import SideNav from './SideNav';
 import './userprofile.css';
+import './dashboard.css'
+import welcome from '../img/welcome.svg';
 
 class Dashboard extends Component {
     render(){
@@ -9,8 +11,17 @@ class Dashboard extends Component {
 
                 <SideNav/>
                 <div className="main">
-                    <div className="dashboard-header" style={{backgroundColor:'#fff',overflow:'hidden'}}>
+                    <div className="dashboard-header" >
                         <p>My Courses</p>
+                    </div>
+
+
+                    <div className="dashboard-section">
+                        <img src={welcome} alt="" className="img-fluid" />
+                        <h3>You Have Not Registered a Course</h3>
+                        <p>You have to register a course to get a certificate,<br></br> Please select the course you wish to be certified with</p>
+                        <button className="fake-btn">Browse Courses</button>
+
                     </div>
                 </div>
 
