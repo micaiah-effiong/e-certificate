5f6a3f5209a402cfd6ae0da1ffad66a6e3ea13f6
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-17 14:17:52 +0100

    added course under api route

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const certRoute = require("./certificate");
 const usersRoute = require("./user");
+const courseRoute = require("./course");
 
 const router = express.Router();
 
 router.use("/certificate", certRoute);
-router.use("/user", usersRoute);
-
+router.use("/users", usersRoute);
+router.use("/courses", courseRoute);
 module.exports = router;
