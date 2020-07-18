ec6fffc500228cb6d2f69e8f98cc95a55178ca1d
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-18 21:49:32 +0100

    Refactor: update controller names, removed dead modules(handlers and middlewares, update crud fuctionality

--- controllers/course.js
+++ controllers/course.js
@@ -1,15 +1,9 @@
-const express = require("express");
-const path = require("path");
-const jwt = require("jsonwebtoken");
-const db = require("../models/index");
-const auth = require("../middlewares/authenticate")(db);
-const stuDetails = require("../middlewares/user-details")(db);
 const { errorResponse, asyncHandler } = require("../handlers/index");
 
 module.exports = function (db) {
   return {
     name: "course",
-    getAllCourses: asyncHandler(async (req, res, next) => {
+    getAll: asyncHandler(async (req, res, next) => {
       let courses = await db.course.findAll();
       res.json({
         success: true,
@@ -17,7 +11,17 @@ module.exports = function (db) {
       });
     }),
 
-    createCourse: asyncHandler(async (req, res, next) => {
+    getSingle: asyncHandler(async (req, res, next) => {
+      let { body } = req;
+      let course = await db.course.findByPk(req.params.id);
+      if (!course) return next(errorResponse("Invalid request", 400));
+      res.json({
+        success: true,
+        data: course.toJSON(),
+      });
+    }),
+
+    create: asyncHandler(async (req, res, next) => {
       let institution = await req.user.getInstitution();
       let course = await institution.createCourse(req.body);
       res.json({
@@ -26,24 +30,24 @@ module.exports = function (db) {
       });
     }),
 
-    updateCourse: asyncHandler(async (req, res, next) => {
+    update: asyncHandler(async (req, res, next) => {
       let { body } = req;
-      let course = await db.findByPk(req.params.id);
+      let course = await db.course.findByPk(req.params.id);
       let courseUpdate = await course.update(body);
 
       res.json({
         success: true,
-        data: courseUpdate,
+        data: courseUpdate.toJSON(),
       });
     }),
 
-    deleteCourse: asyncHandler(async (req, res, next) => {
+    remove: asyncHandler(async (req, res, next) => {
       let { body } = req;
-      let course = await db.findByPk(req.params.id);
+      let course = await db.course.findByPk(req.params.id);
       let courseDelete = await course.delete();
       res.json({
         success: true,
-        data: courseDelete,
+        data: courseDelete.toJSON(),
       });
     }),
   };
