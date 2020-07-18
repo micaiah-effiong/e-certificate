53e08643b0565ebb04a1a8021a155a937d4f638f
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-18 21:51:10 +0100

    rename controllers

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,7 @@ module.exports = function (db) {
   return {
     name: "user",
 
-    getAllUsers: asyncHandler(async (req, res, next) => {
+    getAll: asyncHandler(async (req, res, next) => {
       let users = await db.user.findAll();
       return res.json({
         success: true,
@@ -15,28 +15,29 @@ module.exports = function (db) {
       });
     }),
 
-    getSingleUser: asyncHandler(async (req, res, next) => {
+    getSingle: asyncHandler(async (req, res, next) => {
       let id = parseInt(req.params.id);
       let user = await db.user.findByPk(id);
-      if (!user) return next(errorResponse("user not found", 400));
+      if (!user) return next(errorResponse("Invalid request", 400));
       return res.json({
         success: true,
         data: user.toJSON(),
       });
     }),
 
-    userProfile: (req, res, next) => {
+    userProfile: asyncHandler((req, res, next) => {
       res.json({
         success: true,
-        data: req.user,
+        data: req.user.toJSON(),
       });
-    },
+    }),
 
-    updateSingleUser: asyncHandler(async (req, res, next) => {
+    update: asyncHandler(async (req, res, next) => {
       let id = parseInt(req.params.id);
       let user = await db.user.findOne({ where: { id } });
-      if (!user) return next(errorResponse("No such user", 400));
+      if (!user) return next(errorResponse("Bad request", 400));
       user = await user.update(req.body);
+
       res.json({
         success: true,
         msg: "User has been updated",
@@ -44,10 +45,11 @@ module.exports = function (db) {
       });
     }),
 
-    deleteSingleUser: asyncHandler(async (req, res, next) => {
+    remove: asyncHandler(async (req, res, next) => {
       let id = parseInt(req.params.id);
       let user = await db.user.findOne({ where: { id } });
       if (!user) return next(errorResponse("Bad request", 400));
+
       await db.user.destroy();
       return res.json({
         success: true,
