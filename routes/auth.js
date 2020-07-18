5f07cab5cbeff374019570a3d8c2598e67a7dc10
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-18 21:53:10 +0100

    remove unwanted modules

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const path = require("path");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const db = require("../models/index");
 const {
   auth: { register, login, logout },
 } = require("../controllers/index");
-const stuDetails = require("../middlewares/user-details")(db);
+
 const { errorResponse } = require("../handlers/index");
 const passport = require("../config/passport-config");
 const router = express.Router();
