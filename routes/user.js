358efb1c48d827e6634d9cadfba303839e917159
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-19 21:04:44 +0100

    add endpoints under user for students and institutions

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const studentRouter = require("./student");
+const institutionRouter = require("./institution");
 const {
   user: { getAll, userProfile, getSingle, update, remove },
 } = require("../controllers/index");
 
 const router = express.Router();
+router.use("/student", studentRouter);
+router.use("/institution", institutionRouter);
 
 router.route("/").get(getAll);
 router.route("/profile").get(userProfile);
