6f119f6fc13a361bb50d2714ab54b3d9d3b35799
Akanimorex <akanimoime2016@gmail.com>
2020-07-20 17:49:21 +0100

    dashboard

--- public/src/components/Dashboard.js
+++ public/src/components/Dashboard.js
@@ -13,28 +13,51 @@ class Dashboard extends Component {
     render(){
 
         console.log(this.state.users);
-        
-        return(
-            <div className="continer-fluid">
 
-                <SideNav/>
-                <div className="main">
-                    <div className="dashboard-header" style={{textAlign:'center'}}>
-                        <p>My Courses</p>
+        if (this.state.users === ""){
+
+            return(
+                <div className="continer-fluid">
+    
+                    <SideNav/>
+                    <div className="main">
+                        <div className="dashboard-header" style={{textAlign:'center'}}>
+                            <p>My Courses</p>
+                        </div>
+    
+    
+                        <div className="dashboard-section">
+                            <img src={welcome} alt="" className="img-fluid" />
+                            <h3>You Have Not Registered a Course</h3>
+                            <p>You have to register a course to get a certificate,<br></br> Please select the course you wish to be certified with</p>
+                            <button href="#" className="fake-btn">Browse Courses</button>
+    
+                        </div>
                     </div>
-
-
-                    <div className="dashboard-section">
-                        <img src={welcome} alt="" className="img-fluid" />
-                        <h3>You Have Not Registered a Course</h3>
-                        <p>You have to register a course to get a certificate,<br></br> Please select the course you wish to be certified with</p>
-                        <button href="#" className="fake-btn">Browse Courses</button>
-
-                    </div>
+    
                 </div>
+            )
+            
+        }
+
+        return (
+            <div className="container-fluid">
+                <SideNav/>
+                    <div className="main">
+                        <div className="dashboard-header" style={{textAlign:'center'}}>
+                            <p>My Courses</p>
+                        </div>
+    
+    
+                        <div className="dashboard-section">
+                               
+                        </div>
+                    </div>
 
             </div>
         )
+        
+        
     }
 }
 
