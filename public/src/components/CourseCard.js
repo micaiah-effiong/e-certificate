509740215e2c542aa21e1eccab1f6a1b6c5c93a1
Akanimorex <akanimoime2016@gmail.com>
2020-07-21 14:54:14 +0100

    total new component

--- public/src/components/CourseCard.js
+++ public/src/components/CourseCard.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './dashboard.css'
 
-const CourseCard =({name,level,duration,status})=>{
-    return (
-        <div className="course-card">
-            <div className="top">
-                <h4>{name}</h4>
-                <p>{level}</p>
+const CourseCard =(props)=>{
+    const courses = props.users.map((user)=>{
+        console.log(user);
+        return (
+            <div key={user.id} className="course-card">
+
+                <div className="top">
+                    <h4>{user.courseName}</h4>
+                    <p>{user.courseLevel}</p>
+                </div>
+                <p><span className="fa fa-history"></span>{user.courseDuration} Weeks </p>
+                <p style={{color:'black'}}>status: {user.status}</p>
+
             </div>
-            <p><span className="fa fa-history"></span>{duration} Weeks </p>
-            <p style={{color:'black'}}>status:{status}</p>
-        </div>
-    )
+        )
+
+    }) 
+    return (
+    <div className="card-wrap">
+        {courses}
+    </div>
+)
 }
 
 export default CourseCard;
